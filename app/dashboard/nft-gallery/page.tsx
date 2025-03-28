70831ff453c1717bf30df77bbfa26b471f8d9761
Agbeleshe <danielagbeleshe@gmail.com>
2025-03-28 08:33:02 +0100

    Fixed: Enhance NFT Gallery Page with NFT Collection Card

--- app/dashboard/nft-gallery/page.tsx
+++ app/dashboard/nft-gallery/page.tsx
@@ -1,8 +1,25 @@
 "use client";
 import React, { useState } from "react";
+import nftOne from "@/public/assets/nfts/nftOneRz.png";
+import nftTwo from "@/public/assets/nfts/nftTwoRz.png";
+import nftThree from "@/public/assets/nfts/nftThreRz.png";
+import nftFour from "@/public/assets/nfts/nftFour.png";
+import nftFive from "@/public/assets/nfts/nftFive.png";
+import nftSix from "@/public/assets/nfts/nftSix.png";
+import NftCollectionCard from "@/components/dashboard/NftCollectionCard";
 
 const NftCardGallery = () => {
   const [hasNFTs, setHasNFTs] = useState(true);
+
+  const detailsNft = [
+    { img: nftOne, id: 1, title: "Nova Echo", eth: "1.99", tag: "#1324" },
+    { img: nftTwo, id: 2, title: "Apex Eno", eth: "1.99", tag: "#029" },
+    { img: nftThree, id: 3, title: "Alpha Neon", eth: "1.99", tag: "#4673" },
+    { img: nftSix, id: 4, title: "Goat man", eth: "1.99", tag: "#134" },
+    { img: nftFive, id: 5, title: "Frwnzy punk", eth: "1.99", tag: "#777" },
+    { img: nftFour, id: 6, title: "Billard jynx", eth: "1.99", tag: "#824" },
+  ];
+
   return (
     <div className="pt-12 w-full lg:w-11/12 mx-auto">
       <div className="flex  font-orbitron">
@@ -36,15 +53,20 @@ const NftCardGallery = () => {
         </select>
       </div>
 
-      <div
-        className={`${
-          hasNFTs
-            ? " grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 "
-            : ""
-        } justify-center lg:w-11/12 pt-12 `}
-      >
+      <div className={`${hasNFTs ? "" : ""} justify-center lg:w-11/12 pt-12 `}>
         {hasNFTs ? (
-          "This will be where the cards will be rendered"
+          <div className=" my-5 grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 ">
+            {detailsNft.map((data) => (
+              <div className="card-inview" key={data.id}>
+                <NftCollectionCard
+                  nftImage={data.img}
+                  nftName={data.title}
+                  nftNumber={data.tag}
+                  nftValue={data.eth}
+                />
+              </div>
+            ))}
+          </div>
         ) : (
           <div className="h-screen flex flex-col justify-items-start items-center gap-y-8 text-[#BFBFBF]">
             <h1 className="font-orbitron text-xl">Good to have you here!!</h1>
